301c6b455adf893dafc4a789eea9d3560d35c356
Den Polusmyak <den.polusmyak@gmail.com>
2024-04-12 21:42:01 +0300

    refactor: move analytic to separate file

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,32 +1,11 @@
 import 'server-only';
 
-import { DashboardCard } from '@/ui/components/organisms/DashboardCard';
-import { getSalesData, getUserData, getProductData } from './_actions';
+import { Analytics } from '@/ui/components/organisms/Analytics/Analytics';
 
 export default async function Dashboard() {
-	const [salesData, userData, productData] = await Promise.all([
-		getSalesData(),
-		getUserData(),
-		getProductData(),
-	]);
-
 	return (
-		<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-			<DashboardCard
-				title="Sales"
-				subtitle={`${salesData.numberOfSales} Orders`}
-				content={<p>{salesData.amount}</p>}
-			/>
-			<DashboardCard
-				title="Customers"
-				subtitle={`${userData.averageValuePerUser} Average Value`}
-				content={<p>{userData.userCount}</p>}
-			/>
-			<DashboardCard
-				title="Active Products"
-				subtitle={`${productData.inactiveCount} Inactive`}
-				content={<p>{productData.activeCount}</p>}
-			/>
+		<div>
+			<Analytics />
 		</div>
 	);
 }
---
+++ src/ui/components/organisms/Analytics/Analytics.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { DashboardCard } from '@/ui/components/organisms/DashboardCard';
+import { getSalesData, getUserData, getProductData } from './actions';
+
+export const Analytics = async () => {
+	const [salesData, userData, productData] = await Promise.all([
+		getSalesData(),
+		getUserData(),
+		getProductData(),
+	]);
+
+	return (
+		<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+			<DashboardCard
+				title="Sales"
+				subtitle={`${salesData.numberOfSales} Orders`}
+				content={<p>{salesData.amount}</p>}
+			/>
+			<DashboardCard
+				title="Customers"
+				subtitle={`${userData.averageValuePerUser} Average Value`}
+				content={<p>{userData.userCount}</p>}
+			/>
+			<DashboardCard
+				title="Active Products"
+				subtitle={`${productData.inactiveCount} Inactive`}
+				content={<p>{productData.activeCount}</p>}
+			/>
+		</div>
+	);
+};
