09dd7d635ab07491682be476394ab807424dc34a
Den Polusmyak <den.polusmyak@gmail.com>
2024-04-12 20:20:56 +0300

    chore: improve formatting and format project

--- src/services/libs/prisma.ts
+++ src/services/libs/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+	return new PrismaClient();
 };
 
 declare global {
-  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+	var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
@@ -13,5 +13,5 @@ const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 export default prisma;
 
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.prismaGlobal = prisma;
+	globalThis.prismaGlobal = prisma;
 }
